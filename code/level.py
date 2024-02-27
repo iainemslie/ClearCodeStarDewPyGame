42cbdf2d44eb7846f272342e47644cb65d181cf1
Iain Emslie <iainemslie@gmail.com>
2024-02-27 10:11:48 -0500

    add trees

--- code/level.py
+++ code/level.py
@@ -17,6 +17,7 @@ class Level:
         # sprite groups
         self.all_sprites = CameraGroup()
         self.collision_sprites = pygame.sprite.Group()
+        self.tree_sprites = pygame.sprite.Group()
 
         self.setup()
         self.overlay = Overlay(self.player)
@@ -49,7 +50,7 @@ class Level:
         # trees
         for obj in tmx_data.get_layer_by_name('Trees'):
             Tree((obj.x, obj.y), obj.image,
-                 (self.all_sprites, self.collision_sprites), obj.name)
+                 (self.all_sprites, self.collision_sprites, self.tree_sprites), obj.name)
 
         # wild flowers
         for obj in tmx_data.get_layer_by_name('Decoration'):
@@ -64,8 +65,11 @@ class Level:
         # player
         for obj in tmx_data.get_layer_by_name('Player'):
             if obj.name == 'Start':
-                self.player = Player((obj.x, obj.y), self.all_sprites,
-                                     self.collision_sprites)
+                self.player = Player(pos=(obj.x, obj.y),
+                                     group=self.all_sprites,
+                                     collision_sprites=self.collision_sprites,
+                                     tree_sprites=self.tree_sprites
+                                     )
         Generic(
             pos=(0, 0),
             surf=pygame.image.load(join('graphics', 'world', 'ground.png')),
@@ -87,8 +91,8 @@ class CameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2()
 
     def custom_draw(self, player):
-        self.offset.x = player.rect.centerx - SCREEN_WIDTH // 2
-        self.offset.y = player.rect.centery - SCREEN_HEIGHT // 2
+        self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+        self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
         for layer in LAYERS.values():
             for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
@@ -96,3 +100,16 @@ class CameraGroup(pygame.sprite.Group):
                     offset_rect = sprite.rect.copy()
                     offset_rect.center -= self.offset
                     self.display_surface.blit(sprite.image, offset_rect)
+
+                    # # anaytics
+                    # if sprite == player:
+                    #     pygame.draw.rect(self.display_surface,
+                    #                      'red', offset_rect, 5)
+                    #     hitbox_rect = player.hitbox.copy()
+                    #     hitbox_rect.center = offset_rect.center
+                    #     pygame.draw.rect(self.display_surface,
+                    #                      'green', hitbox_rect, 5)
+                    #     target_pos = offset_rect.center + \
+                    #         PLAYER_TOOL_OFFSET[player.status.split('_')[0]]
+                    #     pygame.draw.circle(self.display_surface,
+                    #                        'blue', target_pos, 5)
