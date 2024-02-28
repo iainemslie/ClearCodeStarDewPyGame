9ec7463531179ed9bc5b8c3a1b73563dce37edf4
Iain Emslie <iainemslie@gmail.com>
2024-02-28 09:13:21 -0500

    planting and growing

--- code/player.py
+++ code/player.py
@@ -75,7 +75,7 @@ class Player(pygame.sprite.Sprite):
             PLAYER_TOOL_OFFSET[self.status.split('_')[0]]
 
     def use_seed(self):
-        pass
+        self.soil_layer.plant_seed(self.target_pos, self.selected_seed)
 
     def import_assets(self):
         self.animations = {
